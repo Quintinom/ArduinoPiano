2777caef693db3cbec1fc6f8e3837d424ba96b59
Quintinom <33523124+Quintinom@users.noreply.github.com>
2017-11-28 16:27:56 +0100

    Add files via upload

---
+++ piano.ino
@@ -0,0 +1,33 @@
+#include <math.h>
+int frequenties = 0;
+int aantalFreq = 0;
+
+void setup()
+{
+  for (int i = 4; i<14; i++)
+  {
+    pinMode(i, INPUT);
+  }
+  pinMode(1, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT); 
+}
+
+void loop()
+{
+  for(int i = 4; i<14; i++){
+    if (digitalRead(i) == HIGH){
+      frequenties = frequenties + Freqfunc(i);
+      aantalFreq = aantalFreq + 1;
+    }
+  }
+  tone(3, frequenties/aantalFreq);
+  frequenties = 0;
+  aantalFreq = 0;
+}
+
+int Freqfunc(double x){
+  int result;
+  result = 464*pow (1.112999, x);
+  return result;
+}
